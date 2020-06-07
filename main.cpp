4bbdf131f08923e58abb757d684a519c29a296ab
mamont92 <gigaslon@yandex.ru>
2020-06-07 12:57:25 +0300

    add dx11 code

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
+#include <tuple>
+#include <array>
+
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
-
-#include <tuple>
+#include <d3d11_1.h>
 
 namespace wnd {
 
@@ -12,19 +14,6 @@ enum class CreateResult{
     RegisterClassFailed,
     CreateInstanceFailed
 };
-
-inline static LPCWSTR toErrorStr(CreateResult val)
-{
-    switch (val) {
-    case CreateResult::OK:
-        return L"OK";
-    case CreateResult::RegisterClassFailed:
-        return L"Failed registering window class!";
-    case CreateResult::CreateInstanceFailed:
-        return L"Failed creation window instance!";
-    }
-    return L"Unknown error!";
-}
 
 constexpr ATOM WINAPI_FAILED = 0;
 static const wchar_t g_className[] = L"DX Windowd Class";
@@ -41,13 +30,41 @@ void showMessageError(T errorVal)
 } //namespace wnd
 
 
+namespace dx {
+
+enum class CreateResult{
+    OK = 0,
+    CreateDeviceAndSwapChainFailed,
+    BackBufferError
+};
+
+struct DXInfo{
+    D3D_DRIVER_TYPE driverType;
+    D3D_FEATURE_LEVEL featureLevel;
+    ID3D11Device * devicePtr;
+    ID3D11DeviceContext * immediateContexPtr;
+    IDXGISwapChain * swapChainPtr;
+    ID3D11RenderTargetView * renderTargetPtr;
+    D3D11_VIEWPORT viewPort;
+};
+
+std::tuple<CreateResult, DXInfo> initD3D(HWND hWnd);
+
+} //namspace dx
+
 //-------------main--func------------------------------------------------------------------------------------
 int WINAPI WinMain(HINSTANCE hInstance, [[maybe_unused]] HINSTANCE hPrevInstance, [[maybe_unused]] LPSTR cmdStr, int cmdShowFlags)
 {
     wnd::Coords wndCoords {0, 0, 800, 600};
-    auto [createResult, wndDesc] = wnd::createWindow(hInstance, cmdShowFlags, L"DX11 Hello!", wndCoords);
-    if (createResult != wnd::CreateResult::OK){
-        wnd::showMessageError(createResult);
+    auto [wndCreateResult, wndHandle] = wnd::createWindow(hInstance, cmdShowFlags, L"DX11 Hello!", wndCoords);
+    if (wndCreateResult != wnd::CreateResult::OK){
+        wnd::showMessageError(wndCreateResult);
+        return 1;
+    }
+
+    auto [d3dCreateResult, dxInfo] = dx::initD3D(wndHandle);
+    if (d3dCreateResult != dx::CreateResult::OK){
+        wnd::showMessageError(d3dCreateResult);
         return 1;
     }
 
@@ -56,6 +73,19 @@ int WINAPI WinMain(HINSTANCE hInstance, [[maybe_unused]] HINSTANCE hPrevInstance
 //-----------------------------------------------------------------------------------------------------------
 
 namespace wnd {
+
+inline static LPCWSTR toErrorStr(CreateResult val)
+{
+    switch (val) {
+    case CreateResult::OK:
+        return L"OK";
+    case CreateResult::RegisterClassFailed:
+        return L"Failed registering window class!";
+    case CreateResult::CreateInstanceFailed:
+        return L"Failed creation window instance!";
+    }
+    return L"Unknown error!";
+}
 
 static LRESULT CALLBACK wndCallback(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
@@ -111,3 +141,93 @@ int startAppLoop()
 }
 
 } //namespace wnd
+
+
+namespace dx {
+
+inline static LPCWSTR toErrorStr(CreateResult val)
+{
+    switch (val) {
+    case CreateResult::OK:
+        return L"OK";
+    case CreateResult::BackBufferError:
+        return L"D3D backBuffer error!";
+    case CreateResult::CreateDeviceAndSwapChainFailed:
+        return L"Failed creation D3D device and swapchain!";
+    }
+    return L"Unknown error!";
+}
+
+std::tuple<CreateResult, DXInfo> initD3D(HWND hWnd)
+{
+    DXInfo resultInfo;
+    HRESULT d3d11Result = E_UNEXPECTED;
+    memset(&resultInfo, 0, sizeof(resultInfo));
+
+    RECT wndCoords;
+    GetClientRect(hWnd, &wndCoords);
+
+    resultInfo.viewPort.Width = static_cast<float>(wndCoords.right - wndCoords.left);
+    resultInfo.viewPort.Height = static_cast<float>(wndCoords.bottom - wndCoords.top);
+    resultInfo.viewPort.MinDepth = 0.0f;
+    resultInfo.viewPort.MaxDepth = 1.0f;
+    resultInfo.viewPort.TopLeftX = 0.0f;
+    resultInfo.viewPort.TopLeftY = 0.0f;
+
+    std::array<D3D_DRIVER_TYPE, 1> d3dDriverTypes {
+        D3D_DRIVER_TYPE_HARDWARE
+    };
+    std::array<D3D_FEATURE_LEVEL, 4> d3dFeatureLevel {
+        D3D_FEATURE_LEVEL_11_1,
+        D3D_FEATURE_LEVEL_11_0,
+        D3D_FEATURE_LEVEL_10_1,
+        D3D_FEATURE_LEVEL_10_0
+    };
+
+    DXGI_SWAP_CHAIN_DESC swapChainDesc;
+    swapChainDesc.BufferCount = 2;
+    swapChainDesc.BufferDesc.Width = wndCoords.right - wndCoords.left;
+    swapChainDesc.BufferDesc.Height = wndCoords.bottom - wndCoords.top;
+    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
+    swapChainDesc.BufferDesc.RefreshRate.Numerator = 60;
+    swapChainDesc.BufferDesc.RefreshRate.Denominator = 1;
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.SampleDesc.Count = 0;
+    swapChainDesc.SampleDesc.Quality = 0;
+    swapChainDesc.Windowed = TRUE;
+
+    UINT deviceFlags = 0;
+
+    for (const auto & driverType : d3dDriverTypes){
+        resultInfo.driverType = driverType;
+        d3d11Result = D3D11CreateDeviceAndSwapChain(/*adapter*/nullptr, driverType, /*software*/nullptr, deviceFlags,
+                                                          d3dFeatureLevel.data(), d3dFeatureLevel.size(), D3D11_SDK_VERSION,
+                                                          &swapChainDesc, &resultInfo.swapChainPtr, &resultInfo.devicePtr,
+                                                          &resultInfo.featureLevel, &resultInfo.immediateContexPtr);
+        if (d3d11Result == E_INVALIDARG) // DX11.0 don't recognize 11_1 feature lvl and return error
+            d3d11Result = D3D11CreateDeviceAndSwapChain(/*adapter*/nullptr, driverType, /*software*/nullptr, deviceFlags,
+                                                         &d3dFeatureLevel[1], (d3dFeatureLevel.size() - 1), D3D11_SDK_VERSION,
+                                                         &swapChainDesc, &resultInfo.swapChainPtr, &resultInfo.devicePtr,
+                                                         &resultInfo.featureLevel, &resultInfo.immediateContexPtr);
+        if (SUCCEEDED(d3d11Result))
+            break;
+    }
+    if (FAILED(d3d11Result))
+        return std::make_tuple(CreateResult::CreateDeviceAndSwapChainFailed, resultInfo);
+
+    ID3D11Texture2D * backBufferPtr = nullptr;
+    auto backBufferResult = resultInfo.swapChainPtr->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<LPVOID*>(&backBufferPtr));
+    if ( FAILED(backBufferResult) || (backBufferPtr == nullptr) )
+        return std::make_tuple(CreateResult::BackBufferError, resultInfo);
+
+    auto rtResult = resultInfo.devicePtr->CreateRenderTargetView(backBufferPtr, nullptr, &resultInfo.renderTargetPtr);
+    if ( FAILED(rtResult) || (resultInfo.renderTargetPtr == nullptr) )
+        return std::make_tuple(CreateResult::BackBufferError, resultInfo);
+
+    resultInfo.immediateContexPtr->OMSetRenderTargets(1, &resultInfo.renderTargetPtr, nullptr);
+    resultInfo.immediateContexPtr->RSSetViewports(1, &resultInfo.viewPort);
+
+    return std::make_tuple(CreateResult::OK, resultInfo);
+}
+
+} //namespace dx
